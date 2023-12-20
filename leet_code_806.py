1a68d75b3b633f669876069f846cf20239310770
kevin-koshy <kevin_koshy@hotmail.com>
2023-12-20 00:39:22 -0500

    LC_806

--- leet_code_806.py
+++ leet_code_806.py
@@ -1,8 +1,21 @@
+from typing import List
+
+
 class Solution:
     def numberOfLines(self, widths: List[int], s: str) -> List[int]:
+        char_array = []
+        lines, width = 1, 0
+        for ch in s:
+            w = widths[ord(ch) - ord('a')]
+            width += w
+            if width > 100:
+                lines += 1
+                width = w
+
+        return [lines, width]
 
 
 result = Solution()
-widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
-s = "abcdefghijklmnopqrstuvwxyz"
+widths = [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+s = "bbbcccdddaaa"
 print(result.numberOfLines(widths, s))
